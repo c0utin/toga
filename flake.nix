f778fc18a71f67c8e20efcc5c37b7e81e1ce261e
c0utin <rafaelcouto111@gmail.com>
2025-04-22 21:21:03 -0300

    [global]toga

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./hosts/toga/configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
+  };
+}
+
