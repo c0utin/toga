c6f590a0a99a091edeb1c9dcf09f1213942d710d
c0utin <rafaelcouto111@gmail.com>
2025-04-27 15:46:12 -0300

    [emacs]nix-mode

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -52,10 +52,18 @@
   :ensure t
   :mode "\\.go\\'"
   :hook (go-mode . lsp-deferred))
-(use-package go-mode
-  :ensure t
-  :mode "\\.go\\'"
-  :hook (go-mode . lsp-deferred))
+
+(use-package nix-mode
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
 
 ;; UI Tweaks
 (tool-bar-mode 0)
