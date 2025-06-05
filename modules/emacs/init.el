f335ed850751277165ca169eab00b8142238c065
c0utin <rafaelcouto111@gmail.com>
2025-06-05 10:04:38 -0300

    [emacs] super as meta

--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -1,4 +1,4 @@
-;; MELPA setup
+;;; MELPA and package setup
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -11,48 +11,58 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; ========================
-;; Appearance & Theme
-;; ========================
+;;; Remap Super key as Meta
+(setq x-super-keysym 'meta)
+
+;;; Theme
 (use-package gruber-darker-theme
   :config
   (load-theme 'gruber-darker t))
 
-;; ========================
-;; Clipboard integration
-;; ========================
+;;; Git integration
+(use-package magit)
+
+;;; Clipboard integration
 (use-package xclip
+  :if (display-graphic-p) ;; Only enable xclip in graphical mode
   :config
   (xclip-mode 1))
 
+;; Function to copy text to system clipboard
 (defun my/copy-to-clipboard (beg end)
-  "Copy region to system clipboard using xclip."
+  "Copy the selected region to the system clipboard."
   (interactive "r")
   (when (use-region-p)
-    (call-process-region beg end "xclip" nil nil nil "-selection" "clipboard")
-    (message "Copied to clipboard")))
+    (let ((text (buffer-substring-no-properties beg end)))
+      (with-temp-buffer
+        (insert text)
+        (if (fboundp 'gui-set-selection)
+            (gui-set-selection 'CLIPBOARD text)
+          ;; fallback to xclip if gui method not available
+          (call-process-region (point-min) (point-max) "xclip" nil nil nil "-selection" "clipboard"))))
+    (message "Copied to clipboard.")))
 
+;; Function to paste from system clipboard
 (defun my/paste-from-clipboard ()
-  "Paste from system clipboard using xclip."
+  "Paste from system clipboard into buffer."
   (interactive)
-  (let ((clipboard-text (shell-command-to-string "xclip -o -selection clipboard")))
-    (insert clipboard-text)))
+  (let ((text (if (fboundp 'gui-get-selection)
+                  (gui-get-selection 'CLIPBOARD)
+                (shell-command-to-string "xclip -o -selection clipboard"))))
+    (insert text)))
 
-(global-set-key (kbd "C-c y") 'my/copy-to-clipboard)
-(global-set-key (kbd "C-c p") 'my/paste-from-clipboard)
+;;; Custom keybindings using general.el
+(use-package general
+  :config
+  (general-define-key
+   :keymaps 'global
+   :prefix "SPC" ;; Space as leader key
+   "y" 'my/copy-to-clipboard
+   "p" 'my/paste-from-clipboard))
 
-;; ========================
-;; Git
-;; ========================
-(use-package magit)
-
-;; ========================
-;; Programming languages
-;; ========================
+;;; Language support
 (use-package zig-mode)
-
 (use-package rust-mode)
-
 (use-package go-mode)
 
 (use-package js2-mode
@@ -60,41 +70,38 @@
          ("\\.jsx\\'" . js2-mode)
          ("\\.ts\\'"  . js2-mode))
   :config
-  (setq js2-basic-offset 2))
+  (setq js2-basic-offset 2)) ;; Indentation size
 
 (use-package nix-mode
-  :mode ("\\.nix\\'" "\\.nix.in\\'" "\\.drv\\'")
-  :commands (nix-repl nix-shell-unpack nix-shell-configure nix-shell-build)
-  :config
-  ;; dummy definitions if missing
-  (unless (fboundp 'nix-read-attr)
-    (defun nix-read-attr (&rest _) (error "nix-read-attr not implemented")))
-  (unless (fboundp 'nix-read-file)
-    (defun nix-read-file (&rest _) (error "nix-read-file not implemented"))))
+  :mode ("\\.nix\\'" "\\.nix.in\\'"))
 
-;; ========================
-;; UI Tweaks
-;; ========================
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
+(use-package nix-drv-mode
+  :ensure nix-mode
+  :mode "\\.drv\\'")
+
+(use-package nix-shell
+  :ensure nix-mode
+  :commands (nix-shell-unpack nix-shell-configure nix-shell-build))
+
+(use-package nix-repl
+  :ensure nix-mode
+  :commands (nix-repl))
+
+;;; UI preferences
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 (column-number-mode 1)
-(global-display-line-numbers-mode)
+(global-display-line-numbers-mode 1)
 
-;; ========================
-;; Input method
-;; ========================
+;;; Input method (for typing special characters)
 (activate-input-method "latin-1-postfix")
 (setq default-input-method "latin-1-postfix")
 
-;; ========================
-;; Dired enhancements
-;; ========================
+;;; Auto-refresh dired
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 
-;; ========================
-;; File settings
-;; ========================
+;;; Disable lock, backup, and autosave files
 (setq create-lockfiles nil)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
